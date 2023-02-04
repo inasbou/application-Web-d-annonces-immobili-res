64eff476cfbe09b611cc019817f47d9326fe30b5
amira791 <ka_bellali@esi.dz>
2023-02-04 23:55:07 +0100

    last commit

--- TP-igl-front-end/src/Pages/MainPage.jsx
+++ TP-igl-front-end/src/Pages/MainPage.jsx
@@ -1,97 +1,62 @@
-import React, { useState, useEffect  } from "react";
-import Annonce from '../Components/MainPage/Annonce'
-import Footer from '../Components/MainPage/Footer'
-import Navbar from '../Components/Navbar/Navbar'
+import React, { useState, useEffect } from "react";
+import Annonce from "../Components/MainPage/Annonce";
+import Filter from "../Components/MainPage/Filter";
+import Footer from "../Components/MainPage/Footer";
+import Navbar from "../Components/Navbar";
 
 const MainPage = () => {
   const [annonces, setAnnonces] = useState([]);
-  
-   //////recuperer annonces //////
-  useEffect(()=>{
-    fetch('http://127.0.0.1:5000/AllAnnonces',{
-      'methods':'GET',
-      headers : {
-        'Content-Type':'application/json'
-      }
-    })
-    
-    .then(response => response.json())
-    .then(response => setAnnonces(response)) 
-   .catch(error => console.log(error))
-    console.log(annonces);
-
-  },[])
-
-
-  
-
-
-  return (
-    <div>
-    
-  
-      <Navbar/>
-          
-  { (annonces.map((annonce) => (
-    <div className="flex flex-row  text-violet-text     ">
-      <Annonce
-      id_annonce={annonce.id_annonce}
-      categorie ={annonce.categorie}
-      type_annonce ={ annonce.type_annonce}
-      prix = {annonce.prix}
-      photo = {annonce.photo}
-      /> </div>
-    )) ) }
-     
-      <Footer/>
-      </div>
-  )
-}
-
-export default MainPage
-
-/*useEffect(() => {
-  fetch("/AllAnnonce").then((res) =>
-
-{
-setAnnonces(res.data);
-console.log(annonces);
-})
-.catch((e) => {
-console.log(e);
-});
-}, []);
-/*
-  const [data, setData] = useState({
-    categorie :"",
-    type_annonce :"" ,
-    surface :0,
-    description :"",
-    prix :0,
-    wilaya :"",
-    commune :"",
-    adresse :"",
-    photo :""
-  }
-  );
+  const [Loading, setLoading] = useState(true);
+  //////recuperer annonces //////
   useEffect(() => {
-    // Using fetch to fetch the api from 
-    // flask server it will be redirected to proxy
-    fetch("/annonce/1").then((res) =>
-        res.json().then((data) => {
-            // Setting a data from api
-            setData({
-              categorie :data.categorie ,
-              type_annonce : data.type_annonce,
-              surface : data.surface,
-              description : data.description,
-              prix : data.prix,
-              wilaya : data.wilaya,
-              commune : data.commune,
-              adresse : data.adresse,
-              photo : data.photo
-            });
-        })
+    fetch("http://127.0.0.1:5000/AllAnnonces", {
+      methods: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((response) => setAnnonces(response))
+      .then(setLoading(false))
+
+      .catch((error) => console.log(error));
+  }, []);
+  /*
+  useEffect(() => {
+    setCurrentAnnonces(
+      annonces.slice(0,18)
     );
-}, []);
-console.log(data);*/
+    
+
+  }, []);
+  console.log(currentAnnonces)
+    
+   */
+  if (Loading) return <div>Loading .....</div>;
+  else
+    return (
+      <div>
+        <Navbar annonces={annonces} setAnnonces={setAnnonces} />
+        <div className="flex flex-col gap-6">
+          <div className="mt-12">
+            <Filter />
+          </div>
+          <div className="flex flex-wrap  gap-6 mt-48 m-5 justify-center ">
+            {annonces.map((annonce) => (
+              <Annonce
+                titre={annonce.titre}
+                id_annonce={annonce.id_annonce}
+                categorie={annonce.categorie}
+                type_annonce={annonce.type_annonce}
+                prix={annonce.prix}
+                photo={annonce.photo}
+              />
+            ))}{" "}
+          </div>
+        </div>
+        <Footer />
+      </div>
+    );
+};
+
+export default MainPage;
